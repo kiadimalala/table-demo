d7809344df3f397cbc8d6c16b378bc300fabf725
Rojoniaina Kiadimalala <kiadimalala.rojoniaina@gmail.com>
2025-11-04 22:57:17 +0300

    remove unused file

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ConnectedSignUpForm from "@/app/components/connected/ConnectedSignUpForm/ConnectedSignUpForm";
-
-export default function Home() {
-  return (
-    <main className="h-[100shv] p-7 grid place-items-center">
-      <ConnectedSignUpForm className="w-full md:max-w-[400px]" />
-    </main>
-  );
-}
